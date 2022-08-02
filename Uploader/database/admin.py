af5238a901de85ecd78e6cf9fdef7c94ba2e2de7
PKDbro <109846008+PKDbro@users.noreply.github.com>
2022-08-02 16:13:35 +0530

    Update admin.py

--- Uploader/database/admin.py
+++ Uploader/database/admin.py
@@ -14,7 +14,7 @@ f = filters.command("status")
 
 s = filters.command("broadcast") & filters.user(Config.OWNER_ID) & filters.reply
 
-@Client.on_message(f)
+@Client.on_message(filters.command("status"))
 async def edited(_, m: Message):
     total, used, free = shutil.disk_usage(".")
     total = humanbytes(total)
